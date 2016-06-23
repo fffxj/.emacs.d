ca00b41235c1479ac85308747332e63a2cbe7e36
fengxiaojie <fengxiaojie1997@gmail.com>
2016-06-23 16:05:00 +0800

    update theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,6 +1,35 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized")
-(load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'dark)    ;;选择solarized dark
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized")
+;(load-theme 'solarized t)
+;(set-frame-parameter nil 'background-mode 'dark)    ;;选择solarized dark
 ;(enable-theme 'solarized)
+
+
+
+(require 'solarized-theme)
+
+;; If you don't customize it, this is the theme you get.
+(setq-default custom-enabled-themes '(sanityinc-solarized-light))
+
+;; Ensure that themes will be applied even if they have not been customized
+;(defun reapply-themes ()
+;  "Forcibly load the themes listed in `custom-enabled-themes'."
+;  (dolist (theme custom-enabled-themes)
+;    (unless (custom-theme-p theme)
+;      (load-theme theme)))
+;  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+;(add-hook 'after-init-hook 'reapply-themes)
+
+;; Toggle between light and dark
+(defun light ()
+  "Activate a light color theme."
+  (interactive)
+  (color-theme-sanityinc-solarized-light))
+
+(defun dark ()
+  "Activate a dark color theme."
+  (interactive)
+  (color-theme-sanityinc-solarized-dark))
+
 
 (provide 'init-theme)
