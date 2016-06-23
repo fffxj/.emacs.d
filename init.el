d8fac54ffb3351fad6f900cf67cbe2d640f53477
fengxiaojie <fengxiaojie1997@gmail.com>
2016-06-23 18:20:29 +0800

    set hippie-expand

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(let ((minver "23.3"))
+  (when (version<= emacs-version "23.1")
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; Package Manager
@@ -12,21 +18,24 @@
 (require 'init-dired)
 (require 'init-smex)
 (require 'init-ibuffer)
-(require 'init-recentf)
-(require 'init-flyspell)
 
 ;; Utils
+(require 'init-recentf)
+(require 'init-uniquify)
+
+;; Editor
+(require 'init-flyspell)
 (require 'init-fonts)
 (require 'init-whitespace)
 
 ;; Programming
 (require 'init-yasnippet)
 (require 'init-auto-complete)
+(require 'init-hippie-expand)
 (require 'init-flycheck)
 
 ;; Color Theme
 (require 'init-theme)
-
 
 
 (provide 'init)
